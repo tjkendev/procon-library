51983a5e985efac4b8694ad10db7e61646f90ff9
jkenull <jake-basu@hotmail.co.jp>
2014-08-20 22:28:17 +0900

    first commit

---
+++ cpp_temp.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<vector>
+#include<map>
+#include<algorithm>
+#include<cstdio>
+#include<functional>
+using namespace std;
+ 
+#define min(a,b) (a>b?b:a)
+#define max(a,b) (a>b?a:b)
+#define rep(i,n) for(int i=0; i<n; ++i)
+#define repl(i,s,n) for(int i=s; i<=n; ++i)
+#define repe(e,obj) for(auto e : obj)
+
+int main() {
+  return 0;
+}
