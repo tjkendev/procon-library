fc394f231d65dc5329cd1f783e6ec40783b11754
jkenull <jake-basu@hotmail.co.jp>
2014-08-24 02:37:53 +0900

    add template

--- cpp_temp.cpp
+++ cpp_temp.cpp
@@ -1,16 +1,36 @@
 #include<iostream>
+#include<string>
 #include<vector>
+#include<queue>
+#include<stack>
 #include<map>
+#include<set>
 #include<algorithm>
-#include<cstdio>
 #include<functional>
+#include<cstdio>
+#include<cmath>
 using namespace std;
  
-#define min(a,b) (a>b?b:a)
-#define max(a,b) (a>b?a:b)
+//#define min(a,b) (a>b?b:a)
+//#define max(a,b) (a>b?a:b)
 #define rep(i,n) for(int i=0; i<n; ++i)
 #define repl(i,s,n) for(int i=s; i<=n; ++i)
 #define repe(e,obj) for(auto e : obj)
+
+#define SP << " " <<
+#define COL << " : " <<
+#define DEB(A) cout << A << endl;
+#define DEBREP(i,n,val) for(int i=0; i<n; ++i) cout << val << " "; cout << endl;
+#define ALL(V) (V).begin(), (V).end()
+#define INF 1000000007
+#define EPS 1e-9
+
+typedef unsigned long long ull;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> P;
+//typedef pair<ll, ll> P;
+typedef <P, vector<P>, greater<P> > pvqueue;
 
 int main() {
   return 0;
---
+++ python_temp.py
@@ -0,0 +1,11 @@
+# Windows IDLE用 テストケースをコピペで処理できるように
+# (C-dで入力終了)
+# submit時には削除する
+import sys
+def raw_input_generator():
+    for s in sys.stdin.readlines():
+        for ss in s.strip().split('\n'):
+            yield ss.strip()
+raw_gen = raw_input_generator()
+raw_input = lambda: raw_gen.next()
+input = lambda: eval(raw_gen.next())
