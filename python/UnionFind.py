7b8f83f42be4ad186121a3baef5fd258937ac9b9
jake jaga <jake-basu@hotmail.co.jp>
2015-04-27 21:43:40 +0900

    add UnionFind.py

---
+++ python/UnionFind.py
@@ -0,0 +1,20 @@
+class UnionFind:
+    def __init__(self, n):
+        self.n = n
+        self.data = [0]*n
+        for i in xrange(n):
+            self.data[i] = i
+    def root(self, x):
+        if self.data[x] == x:
+            return x
+        self.data[x] = self.root(self.data[x])
+        return self.data[x]
+    def unite(self, x, y):
+        rx = self.root(x)
+        ry = self.root(y)
+        if rx<ry:
+            self.data[ry] = rx
+        elif rx>ry:
+            self.data[rx] = ry
+    def match(self, x, y):
+        return self.root(x)==self.root(y)
