02ba0f523fcfcbb82859d848be020a65ed102a45
jake jaga <jake-basu@hotmail.co.jp>
2016-06-03 23:36:45 +0900

    add RollingHash in Python

--- python/String.py
+++ python/String.py
@@ -164,3 +164,18 @@ def LCP(s, n, sa):
             h += 1
         lcp[rank[i] - 1] = h
     return lcp
+
+
+# 1-dimension Rolling Hash
+class RollingHash:
+    def __init__(self, s, base, MOD):
+        self.s = s
+        self.l = l = len(s)
+        self.base = base
+        self.MOD = MOD
+        self.h = h = [0]*(l + 1)
+        for i in xrange(l):
+            h[i+1] = (h[i] * base + ord(s[i])) % MOD
+    def get(self, l, r):
+        MOD = self.MOD
+        return ((self.h[r] - self.h[l]*pow(self.base, r-l, MOD) + MOD)) % MOD
