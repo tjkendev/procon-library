2ac634231ef32a97ca0f7451fea9ff3352d9a12c
jakejaga <jake-basu@hotmail.co.jp>
2017-10-14 20:45:36 +0900

    add BIT and RH

--- python/SegmentTree.py
+++ python/SegmentTree.py
@@ -44,3 +44,64 @@ class SegmentTree:
 
     def get(self, a, b):
         return self.__get(0, self.n, 0, a, b)
+
+# Binary Indexed Tree (Fenwick Tree)
+class BIT:
+    def __init__(self, n):
+        self.n = n
+        self.data = [0]*(n+1)
+        self.el = [0]*(n+1)
+    def sum(self, i):
+        s = 0
+        while i > 0:
+            s += self.data[i]
+            i -= i & -i
+        return s
+    def add(self, i, x):
+        # assert i > 0
+        self.el[i] += x
+        while i <= self.n:
+            self.data[i] += x
+            i += i & -i
+    def get(self, i, j=None):
+        if j is None:
+            return self.el[i]
+        return self.sum(j) - self.sum(i)
+
+# Binary Index Tree (2-dimension)
+class BIT2:
+    # H*W
+    def __init__(self, h, w):
+        self.w = w
+        self.h = h
+        self.data = [{} for i in xrange(h+1)]
+
+    # O(logH*logW)
+    def sum(self, i, j):
+        s = 0
+        data = self.data
+        while i > 0:
+            el = data[i]
+            k = j
+            while k > 0:
+                s += el.get(k, 0)
+                k -= k & -k
+            i -= i & -i
+        return s
+
+    # O(logH*logW)
+    def add(self, i, j, x):
+        w = self.w; h = self.h
+        data = self.data
+        while i <= h:
+            el = data[i]
+            k = j
+            while k <= w:
+                el[k] = el.get(k, 0) + x
+                k += k & -k
+            i += i & -i
+
+    # [x0, x1) x [y0, y1)
+    def range_sum(self, x0, x1, y0, y1):
+        return self.sum(x1, y1) - self.sum(x1, y0) - self.sum(x0, y1) + self.sum(x0, y0)
+
--- python/String.py
+++ python/String.py
@@ -263,3 +263,44 @@ class RollingHash:
     def get(self, l, r):
         MOD = self.MOD
         return ((self.h[r] - self.h[l]*pow(self.base, r-l, MOD) + MOD)) % MOD
+
+# Random Rolling Hash
+import random, math
+random.seed()
+def gen(a, b, num):
+    result = set()
+    while 1:
+        while 1:
+            v = random.randint(a, b)/2*2+1
+            if v not in result:
+                break
+        for x in xrange(3, int(math.sqrt(v))+1, 2):
+            if v % x == 0:
+                break
+        else:
+            result.add(v)
+            if len(result) == num:
+                break
+    return result
+class RH():
+    def __init__(self, s, base, mod):
+        self.base = base
+        self.mod = mod
+        self.rev = pow(base, mod-2, mod)
+
+        l = len(s)
+        self.h = h = [0]*(l+1)
+        tmp = 0
+        for i in xrange(l):
+            num = ord(s[i])
+            tmp = (tmp*base + num) % mod
+            h[i+1] = tmp
+    def calc(self, l, r):
+        return (self.h[r] - self.h[l] + self.mod) * pow(self.rev, l, self.mod) % self.mod
+class RRH():
+    def __init__(self, s, num=10):
+        param = (2, 10**3, num)
+        MOD = 10**9+7
+        self.rhs = [RH(s, p, MOD) for p in gen(*param)]
+    def calc(self, l, r):
+        return [rh.calc(l, r) for rh in self.rhs]
