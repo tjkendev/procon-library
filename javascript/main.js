9170dd64c99f73779c1cb3976a740441f6256663
jakejaga <jake-basu@hotmail.co.jp>
2017-12-14 01:53:13 +0900

    update

--- javascript/main.js
+++ javascript/main.js
@@ -28,20 +28,26 @@ Array.prototype.smap = function() { return this.map((x)=>String(x)); }
 Array.prototype.ffill = function(cb) {
   for(var i=0; i<this.length; ++i) { this[i] = cb(i); } return this;
 }
-Array.prototype.max = function() {
+Array.prototype.max = function(cb) {
   if(this.length === 0) return undefined;
-  res = this[0];
-  for(var i=1; i<this.length; ++i) if(res < this[i]) res = this[i];
+  res = cb ? cb(this[0], 0) : this[0];
+  for(var i=1; i<this.length; ++i){ var val = cb ? cb(this[i], i) : this[i]; if(res < val) res = val; }
   return res;
 }
-Array.prototype.min = function() {
+Array.prototype.min = function(cb) {
   if(this.length === 0) return undefined;
-  res = this[0];
-  for(var i=1; i<this.length; ++i) if(this[i] < res) res = this[i];
+  res = cb ? cb(this[0], 0) : this[0];
+  for(var i=1; i<this.length; ++i){ var val = cb ? cb(this[i], i) : this[i]; if(val < res) res = val; }
+  return res;
+}
+Array.prototype.sum = function(cb) {
+  if(this.length === 0) return undefined;
+  res = cb ? cb(this[0], 0) : this[0];
+  for(var i=1; i<this.length; ++i) res += cb ? cb(this[i], i) : this[i];
   return res;
 }
 
-Number.prototype.times = function(cb) { for(var i=0; i<this; ++i) cb(i); return this; }
+Number.prototype.times = function(cb) { res = []; for(var i=0; i<this; ++i) res.push(cb ? cb(i) : i); return res; }
 
 Object.prototype.forEach = function(cb) { Object.keys(this).forEach(cb); }
-Object.prototype.map = function(cb) { Object.keys(this).map(cb); }
+Object.prototype.map = function(cb) { return Object.keys(this).map(cb); }
--- python/Graph.py
+++ python/Graph.py
@@ -47,6 +47,37 @@ def bridge_finding(G, N, start=0):
         bG[u].append(start)
     return bG#, bV
 
+# 単純に橋となる辺を返す
+def bridge(G, N):
+    result = set()
+    label = [None]*N
+    gen = 0
+    cost = [0]*N
+    def dfs(u, p):
+        nonlocal gen
+        res = 0
+        for v in G[u]:
+            if v == p:
+                continue
+            if label[v] is not None:
+                if label[v] < label[u]:
+                    cost[v] += 1
+                    res += 1
+            else:
+                label[v] = gen; gen += 1
+                r = dfs(v, u)
+                if r == 0:
+                    result.add((u, v) if u < v else (v, u))
+                res += r
+        res -= cost[u]
+        return res
+    for v in range(N):
+        if not label[v]:
+            label[v] = gen; gen += 1
+            r = dfs(v, -1)
+            assert r == 0, r
+    return result
+
 # 関節点(Articulation Points)
 def get_articulation_points(G, N, start=0):
     v_min = [0]*N; order = [None]*N
