908c7cd27b6f9c21bedfe3990f0e48172573364b
tjkendev <jake-basu@hotmail.co.jp>
2018-10-03 00:11:21 +0900

    add some graph algorithm pages

---
+++ python/graph/topological_sort.py
@@ -0,0 +1,20 @@
+# V: 頂点数
+# G[v] = [w, ...]:
+#    有向グラフ上の頂点vから到達できる頂点w
+# deg[v]:
+#    頂点vに到達できる頂点の数
+
+from collections import deque
+ans = list(v for v in range(V) if deg[v]==0)
+deq = deque(ans)
+used = [0]*V
+
+while deq:
+    v = deq.popleft()
+    for t in g[v]:
+        deg[t] -= 1
+        if deg[t]==0:
+            deq.append(t)
+            ans.append(t)
+
+# ans: トポロジカル順序に並べた頂点
---
+++ python/graph/tree_diameter.py
@@ -0,0 +1,23 @@
+# N: 木Tの頂点数
+# G[u] = [(w, c), ...]:
+#   頂点uに隣接する頂点wとそれを繋ぐ辺の長さc
+
+from collections import deque
+def bfs(s):
+    dist = [None]*N
+    que = deque([s])
+    dist[s] = 0
+    while que:
+        v = que.popleft()
+        d = dist[v]
+        for w, c in G[v]:
+            if dist[w] is not None:
+                continue
+            dist[w] = d + c
+            que.append(w)
+    d = max(dist)
+    return dist.index(d), d
+u, _ = bfs(0)
+v, d = bfs(u)
+
+# パスu-vがこの木Tの直径(長さd)
