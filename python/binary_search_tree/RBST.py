a1e8c69eaa55f2cd051a5c3d668094d706a916a1
tjkendev <jake-basu@hotmail.co.jp>
2018-10-15 00:12:31 +0900

    update BST pages

---
+++ python/binary_search_tree/RBST.py
@@ -0,0 +1,129 @@
+import random
+random.seed()
+randint = random.randint
+
+# merge tree l with tree r
+def merge(l, r):
+    if not l or not r:
+        return l or r
+
+    #st = []; dr = []
+    z = y = [None]; d = 0
+    while l and r:
+        lc = l[3]; rc = r[3]
+        if randint(1, lc+rc) <= lc:
+            y[d] = l
+            l[3] = lc+rc
+            y = l; d = 1
+            l = l[1]
+        else:
+            y[d] = r
+            r[3] = lc+rc
+            y = r; d = 0
+            r = r[0]
+        #st.append(y)
+        #dr.append(d)
+    y[d] = l or r
+    #while st:
+    #    x = st.pop(); d = dr.pop()
+    return z[0]
+
+# split tree using stack info
+def __split(st, dr):
+    l = None; r = None
+    st.reverse(); dr.reverse()
+    for x, d in zip(st, dr):
+        if d:
+            x[1] = q = l
+            p = x[0]
+            l = x
+        else:
+            x[0] = p = r
+            q = x[1]
+            r = x
+        x[3] = (p[3] if p else 0) + (q[3] if q else 0) + 1
+    return l, r
+
+# split tree t into two trees of size k and |t|-k
+def split(t, k):
+    if not t:
+        return None, None
+    x = t
+    st = []; dr = []
+    while x:
+        l = x[0]
+        c = (l[3] if l else 0) + 1
+
+        st.append(x)
+        if c <= k:
+            k -= c
+            x = x[1]
+            d = 1
+        else:
+            x = x[0]
+            d = 0
+        dr.append(d)
+    return __split(st, dr)
+
+# insert node with key = val
+def insert(t, val):
+    x = t
+    st = []; dr = []
+    while x:
+        st.append(x)
+        if x[2] == val:
+            return t
+        d = (x[2] < val)
+        dr.append(d)
+        x = x[d]
+
+    l, r = __split(st, dr)
+
+    # [<left>, <right>, <key>, <count>]
+    nd = [None, None, val, 1]
+
+    # merge tree l and node nd
+    x = l; y = None
+    while x:
+        if not randint(0, x[3]):
+            if y:
+                y[1] = nd
+            else:
+                l = nd
+            nd[0] = x
+            nd[3] = x[3]+1
+            break
+        y = x
+        x[3] += 1
+        x = x[1]
+    else:
+        if y:
+            y[1] = nd
+        else:
+            l = nd
+    return merge(l, r)
+
+# delete node with key = val
+def delete(t, val):
+    x = t
+    st = []; dr = []
+    y = None
+    while x:
+        st.append(x)
+        d = x[2] <= val
+        dr.append(d)
+        x = x[d]
+    l, r = __split(st, dr)
+
+    st = []; dr = []
+    x = l
+    while x:
+        st.append(x)
+        d = (x[2] < val)
+        dr.append(d)
+        x = x[d]
+    l, m = __split(st, dr)
+
+    #assert m[2] == val
+
+    return merge(l, r)
--- python/binary_search_tree/treap.py
+++ python/binary_search_tree/treap.py
@@ -12,27 +12,24 @@ def rotate(nd, d):
         e = c[0]
         nd[0] = c[1]
         c[1] = nd
- 
+
     r = c[4] = nd[4]
     nd[4] = r - (e[4] if e else 0) - 1
     return c
- 
- 
+
 # insert node with key = val and priority = pri
-root = None
-def insert(val, pri):
-    global root
+def insert(t, val, pri):
     st = []
     dr = []
-    x = root
+    x = t
     while x:
         st.append(x)
         if x[2] == val:
-            return
+            return t
         d = (x[2] < val)
         dr.append(d)
         x = x[d]
- 
+
     # [<left>, <right>, <key>, <priority>, <count>]
     nd = [None, None, val, pri, 1]
     while st:
@@ -43,11 +40,12 @@ def insert(val, pri):
             break
         rotate(x, d)
     else:
-        root = nd
- 
+        return nd
+
     for x in st:
         x[4] += 1
- 
+    return t
+
 # delete node nd
 def __delete(nd):
     st = []; dr = []
@@ -62,12 +60,11 @@ def __delete(nd):
         x[d] = nd
         x[4] -= 1
     return x
- 
+
 # delete node with key = val
-def delete(val):
-    global root
-    x = root
- 
+def delete(t, val):
+    x = t
+
     st = []
     y = None
     while x:
@@ -78,19 +75,83 @@ def delete(val):
         x = x[d]
     else:
         return
- 
+
     if y:
         y[d] = __delete(x)
         for x in st:
             x[4] -= 1
-    else:
-        root = __delete(x)
- 
+        return t
+    return __delete(x)
+
 # find node with key = val
-def find(val):
-    x = root
+def find(t, val):
+    x = t
     while x:
         if val == x[2]:
             return 1
         x = x[x[2] < val]
     return 0
+
+# merge tree l with tree r
+def merge(l, r):
+    if not l:
+        return r
+    if not r:
+        return l
+    nd = [l, r, None, None, l[4]+r[4]+1]
+    return __delete(nd)
+
+# split tree t into two trees of size k and |t|-k
+def split(t, k):
+    x = t
+    if not 0 < k < x[4]:
+        return t, None
+    st = []; dr = []
+    while x:
+        l = x[0]
+
+        c = (l[4] if l else 0) + 1
+        if c == k:
+            break
+        y = x
+        st.append(y)
+
+        if c < k:
+            k -= c
+            x = x[1]
+            d = 1
+        else:
+            x = l
+            d = 0
+        dr.append(d)
+    # x is k-th element in tree t
+    #assert x
+    st.append(x)
+    dr.append(1)
+    x = x[1]
+    while x:
+        st.append(x)
+        dr.append(0)
+        x = x[0]
+    nd = [None, None, None, None, 1]
+    while st:
+        x = st.pop(); d = dr.pop()
+        x[d] = nd
+        x[4] += 1
+        rotate(x, d)
+    return nd[0], nd[1]
+
+# for debug
+def debug(t):
+    def dfs(t, k):
+        cc = 1
+        if t[0]:
+            cc += dfs(t[0], k+1)
+        print(" "*k, "%d (%d) %d" % (t[2], t[3], t[4]))
+        if t[1]:
+            cc += t[1] and dfs(t[1], k+1)
+        assert t[4] == cc
+        return cc
+    if not t:
+        return 0
+    return dfs(t, 0)
