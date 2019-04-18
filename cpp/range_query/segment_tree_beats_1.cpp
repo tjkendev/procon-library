6c8d6cb330391ebc3be45e1e9b195c80b6e90c28
tjkendev <jake-basu@hotmail.co.jp>
2019-04-19 08:53:36 +0900

    change some pages

---
+++ cpp/range_query/segment_tree_beats_1.cpp
@@ -0,0 +1,119 @@
+#include<algorithm>
+#include<random>
+#include<iostream>
+using namespace std;
+using ll = long long;
+
+#define N 10003
+
+class SegmentTree {
+  const ll inf = 1e18;
+  int n, n0;
+  ll max_v[4*N], smax_v[4*N];
+  ll sum[4*N], max_c[4*N];
+
+  void push(int k) {
+    if(max_v[k] < max_v[2*k+1]) {
+      sum[2*k+1] += (max_v[k] - max_v[2*k+1]) * max_c[2*k+1];
+      max_v[2*k+1] = max_v[k];
+    }
+    if(max_v[k] < max_v[2*k+2]) {
+      sum[2*k+2] += (max_v[k] - max_v[2*k+2]) * max_c[2*k+2];
+      max_v[2*k+2] = max_v[k];
+    }
+  }
+
+  void update(int k) {
+    sum[k] = sum[2*k+1] + sum[2*k+2];
+
+    if(max_v[2*k+1] < max_v[2*k+2]) {
+      max_v[k] = max_v[2*k+2];
+      max_c[k] = max_c[2*k+2];
+      smax_v[k] = max(max_v[2*k+1], smax_v[2*k+2]);
+    } else if(max_v[2*k+1] > max_v[2*k+2]) {
+      max_v[k] = max_v[2*k+1];
+      max_c[k] = max_c[2*k+1];
+      smax_v[k] = max(smax_v[2*k+1], max_v[2*k+2]);
+    } else {
+      max_v[k] = max_v[2*k+1];
+      max_c[k] = max_c[2*k+1] + max_c[2*k+2];
+      smax_v[k] = max(smax_v[2*k+1], smax_v[2*k+2]);
+    }
+  }
+
+  void _update_min(ll x, int a, int b, int k, int l, int r) {
+    if(b <= l || r <= a || max_v[k] <= x) {
+      return;
+    }
+    if(a <= l && r <= b && smax_v[k] < x) {
+      sum[k] += (x - max_v[k]) * max_c[k];
+      max_v[k] = x;
+      return;
+    }
+
+    push(k);
+    _update_min(x, a, b, 2*k+1, l, (l+r)/2);
+    _update_min(x, a, b, 2*k+2, (l+r)/2, r);
+    update(k);
+  }
+
+  ll _query_max(int a, int b, int k, int l, int r) {
+    if(b <= l || r <= a) {
+      return 0;
+    }
+    if(a <= l && r <= b) {
+      return max_v[k];
+    }
+    push(k);
+    ll lv = _query_max(a, b, 2*k+1, l, (l+r)/2);
+    ll rv = _query_max(a, b, 2*k+2, (l+r)/2, r);
+    return max(lv, rv);
+  }
+
+  ll _query_sum(int a, int b, int k, int l, int r) {
+    if(b <= l || r <= a) {
+      return 0;
+    }
+    if(a <= l && r <= b) {
+      return sum[k];
+    }
+    push(k);
+    ll lv = _query_sum(a, b, 2*k+1, l, (l+r)/2);
+    ll rv = _query_sum(a, b, 2*k+2, (l+r)/2, r);
+    return lv + rv;
+  }
+
+public:
+  SegmentTree(int n) {
+    SegmentTree(n, nullptr);
+  }
+
+  SegmentTree(int n, ll *a) : n(n) {
+    n0 = 1;
+    while(n0 < n) n0 <<= 1;
+
+    for(int i=0; i<n; ++i) {
+      max_v[n0-1+i] = sum[n0-1+i] = (a != nullptr ? a[i] : 0);
+      smax_v[n0-1+i] = -inf;
+      max_c[n0-1+i] = 1;
+    }
+
+    for(int i=n; i<n0; ++i) {
+      max_v[n0-1+i] = smax_v[n0-1+i] = -inf;
+      sum[n0-1+i] = max_c[n0-1+i] = 0;
+    }
+    for(int i=n0-2; i>=0; i--) update(i);
+  }
+
+  void update_min(int a, int b, ll x) {
+    return _update_min(x, a, b, 0, 0, n0);
+  }
+
+  ll query_max(int a, int b) {
+    return _query_max(a, b, 0, 0, n0);
+  }
+
+  ll query_sum(int a, int b) {
+    return _query_sum(a, b, 0, 0, n0);
+  }
+};
