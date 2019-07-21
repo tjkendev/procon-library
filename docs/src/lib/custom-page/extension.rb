ed02a76905049716feed7badef48467b9e886fd2
tjkendev <jake-basu@hotmail.co.jp>
2019-07-21 14:45:41 +0900

    add docinfo-last.html

--- docs/src/lib/custom-page/extension.rb
+++ docs/src/lib/custom-page/extension.rb
@@ -8,20 +8,24 @@ require 'cgi'
 
 class CustomPage < Extensions::Postprocessor
   def process document, output
-
-    # :no-copy: が定義されている場合はボタンを生成しない
-    if document.attributes['no-copy']
-      return output
-    end
-
     doc = Nokogiri::HTML.parse(output)
 
-    # add "Copy to clipboard" button
-    doc.search(".listingblock").each_with_index{|el, i|
-      code = el.search("code")
-      code.add_class("code-idx-%d" % i)
-      el.prepend_child '<button type="button" class="copy-btn" data-clipboard-target=".code-idx-%d">Copy to clipboard</button>' % i
-    }
+    if !document.attributes.key?('no-copy')
+      # :no-copy: が定義されていなければボタンを生成する
+
+      # add "Copy to clipboard" button
+      doc.search(".listingblock").each_with_index{|el, i|
+        code = el.search("code")
+        code.add_class("code-idx-%d" % i)
+        el.prepend_child '<button type="button" class="copy-btn" data-clipboard-target=".code-idx-%d">Copy to clipboard</button>' % i
+      }
+    end
+
+    # docinfo-last.html
+    docinfo_last = document.docinfo "last"
+    if !docinfo_last.empty?
+      doc.at("body").add_child(docinfo_last)
+    end
 
     doc.to_html
   end
