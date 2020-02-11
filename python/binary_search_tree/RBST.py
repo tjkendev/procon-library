9c79ad5987ec9df7ef3f8f4b1c905f3995edfee9
tjkendev <jake-basu@hotmail.co.jp>
2020-02-11 20:45:14 +0900

    update RBST in Python

--- python/binary_search_tree/RBST.py
+++ python/binary_search_tree/RBST.py
@@ -1,129 +1,166 @@
 import random
+
 random.seed()
-randint = random.randint
+class RBST:
+    def __init__(self):
+        self.root = None
 
-# merge tree l with tree r
-def merge(l, r):
-    if not l or not r:
-        return l or r
+    # find a node with a key x
+    def find(self, x):
+        node = self.root
+        while node:
+            v = node[2]
+            if v == x:
+                return 1
+            node = node[v < x]
+        return 0
 
-    #st = []; dr = []
-    z = y = [None]; d = 0
-    while l and r:
-        lc = l[3]; rc = r[3]
-        if randint(1, lc+rc) <= lc:
-            y[d] = l
-            l[3] = lc+rc
-            y = l; d = 1
-            l = l[1]
-        else:
-            y[d] = r
-            r[3] = lc+rc
-            y = r; d = 0
-            r = r[0]
-        #st.append(y)
-        #dr.append(d)
-    y[d] = l or r
-    #while st:
-    #    x = st.pop(); d = dr.pop()
-    return z[0]
-
-# split a tree using stack info
-def __split(st, dr):
-    l = None; r = None
-    st.reverse(); dr.reverse()
-    for x, d in zip(st, dr):
-        if d:
-            x[1] = q = l
-            p = x[0]
-            l = x
-        else:
-            x[0] = p = r
-            q = x[1]
-            r = x
-        x[3] = (p[3] if p else 0) + (q[3] if q else 0) + 1
-    return l, r
-
-# split tree t into two trees of size k and |t|-k
-def split(t, k):
-    if not t:
-        return None, None
-    x = t
-    st = []; dr = []
-    while x:
-        l = x[0]
-        c = (l[3] if l else 0) + 1
-
-        st.append(x)
-        if c <= k:
-            k -= c
-            x = x[1]
-            d = 1
-        else:
-            x = x[0]
-            d = 0
-        dr.append(d)
-    return __split(st, dr)
-
-# insert a node with key = val
-def insert(t, val):
-    x = t
-    st = []; dr = []
-    while x:
-        st.append(x)
-        if x[2] == val:
-            return t
-        d = (x[2] < val)
-        dr.append(d)
-        x = x[d]
-
-    l, r = __split(st, dr)
-
-    # [<left>, <right>, <key>, <count>]
-    nd = [None, None, val, 1]
-
-    # merge tree l and node nd
-    x = l; y = None
-    while x:
-        if not randint(0, x[3]):
-            if y:
-                y[1] = nd
+    # find the k-th key in a tree
+    def at(self, k):
+        node = self.root
+        k += 1
+        while 1:
+            v = (node[0][3]+1 if node[0] else 1)
+            if v == k:
+                return node[2]
+            if k < v:
+                node = node[0]
             else:
-                l = nd
-            nd[0] = x
-            nd[3] = x[3]+1
-            break
-        y = x
-        x[3] += 1
-        x = x[1]
-    else:
-        if y:
-            y[1] = nd
+                k -= v
+                node = node[1]
+        return -1
+
+    # insert a node with a key x
+    def insert(self, x):
+        # before insert(x), the tree must not contain a key x
+        rand = random.random
+        new_node = [None, None, x, 1]
+        if not self.root:
+            self.root = new_node
+            return
+        node = self.root
+        prv = None
+        while node:
+            if node[3] == int(rand() * (node[3]+1)):
+                break
+            node[3] += 1
+            prv = node
+            node = node[node[2] < x]
+        if prv:
+            prv[prv[2] < x] = new_node
         else:
-            l = nd
-    return merge(l, r)
+            self.root = new_node
 
-# delete a node with key = val
-def delete(t, val):
-    x = t
-    st = []; dr = []
-    y = None
-    while x:
-        st.append(x)
-        d = x[2] <= val
-        dr.append(d)
-        x = x[d]
-    l, r = __split(st, dr)
+        left = right = new_node
+        st = []
+        while node:
+            st.append(node)
+            if node[2] < x:
+                left[1] = left = node
+                node = node[1]
+            else:
+                right[0] = right = node
+                node = node[0]
+        left[1] = right[0] = None
+        new_node[0], new_node[1] = new_node[1], new_node[0]
 
-    st = []; dr = []
-    x = l
-    while x:
-        st.append(x)
-        d = (x[2] < val)
-        dr.append(d)
-        x = x[d]
-    l, m = __split(st, dr)
+        st.reverse()
+        for node in st:
+            l, r = node[:2]
+            node[3] = (l[3] if l else 0) + (r[3] if r else 0) + 1
+        l, r = new_node[:2]
+        new_node[3] = (l[3] if l else 0) + (r[3] if r else 0) + 1
 
-    #assert m[2] == val
+    # remove a node with key x
+    def remove(self, x):
+        # before remove(x), the tree must contain a key x
+        rand = random.random
+        node = self.root
+        prt = None
+        while node[2] != x:
+            node[3] -= 1
+            prt = node
+            node = node[node[2] < x]
 
-    return merge(l, r)
+        cur = top = prt
+        if prt:
+            prv_d = (prt[1] == node)
+        else:
+            cur = top = [None]
+            prv_d = 0
+        left, right = node[:2]
+        st = []; push = st.append
+        while left and right:
+            a = left[3]; b = right[3]
+            if int(rand() * (a+b)) < a:
+                push(left)
+                cur[prv_d] = cur = left
+                left = left[1]
+                prv_d = 1
+            else:
+                push(right)
+                cur[prv_d] = cur = right
+                right = right[0]
+                prv_d = 0
+        rest = left or right
+        cur[prv_d] = rest
+        if not prt:
+            self.root = top[0]
+
+        st.reverse()
+        for node in st:
+            l, r = node[:2]
+            node[3] = (l[3] if l else 0) + (r[3] if r else 0) + 1
+
+    # pop the k-th key
+    def pop(self, k):
+        # when pop(x), the tree should contain the k-th element
+        rand = random.random
+        node = self.root
+        prt = None
+        k += 1
+        while 1:
+            l = node[0]
+            v = (l[3]+1 if l else 1)
+            if v == k:
+                break
+            prt = node
+            node[3] -= 1
+            if k < v:
+                node = node[0]
+            else:
+                k -= v
+                node = node[1]
+        r_node = node
+
+        cur = top = prt
+        if prt:
+            prv_d = (prt[1] == node)
+        else:
+            cur = top = [None]
+            prv_d = 0
+        left, right = node[:2]
+        st = []; push = st.append
+        while left and right:
+            a = left[3]; b = right[3]
+            if int(rand() * (a+b)) < a:
+                push(left)
+                cur[prv_d] = cur = left
+                left = left[1]
+                prv_d = 1
+            else:
+                push(right)
+                cur[prv_d] = cur = right
+                right = right[0]
+                prv_d = 0
+        rest = left or right
+        cur[prv_d] = rest
+        if not prt:
+            self.root = top[0]
+
+        st.reverse()
+        for node in st:
+            l, r = node[:2]
+            node[3] = (l[3] if l else 0) + (r[3] if r else 0) + 1
+
+        return r_node[2]
