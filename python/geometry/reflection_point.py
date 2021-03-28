0324c4e3c027fccc2420dd6275b2dc12bd5c35f8
tjkendev <jake-basu@hotmail.co.jp>
2021-03-28 19:47:56 +0900

    update reflection_point page in Python

--- python/geometry/reflection_point.py
+++ python/geometry/reflection_point.py
@@ -1,17 +1,22 @@
+def dot3(a, b, c):
+    x0, y0 = a; x1, y1 = b; x2, y2 = c
+    return (x1 - x0)*(x2 - x0) + (y1 - y0)*(y2 - y0)
+def dist2(a, b):
+    x0, y0 = a; x1, y1 = b
+    return (x0 - x1)**2 + (y0 - y1)**2
 def reflection(line, point):
-    x0, y0, x1, y1 = line
-    p, q = point
-    x1 -= x0; y1 -= y0
-    p -= x0; q -= y0
-    cv = p*x1 + q*y1
-    sv = p*y1 - q*x1
-    cv2 = cv**2 - sv**2
-    sv2 = 2*cv*sv
-    dd = (p**2 + q**2)*(x1**2 + y1**2)
-    if dd == 0:
-        return x0 + p, y0 + q
-    return x0 + (cv2 * p - sv2 * q) / dd, y0 + (sv2 * p + cv2 * q) / dd
+    p0, p1 = line
+    dv = dot3(p0, p1, point)
+    dd = dist2(p0, p1)
+
+    x0, y0 = p0; x1, y1 = p1
+    xp, yp = point
+
+    xm = x0 + dv * (x1 - x0) / dd
+    ym = y0 + dv * (y1 - y0) / dd
+
+    return xp + 2*(xm - xp), yp + 2*(ym - yp)
 
 # example
-print(reflection((0, 2, 2, 1), (0, 0)))
+print(reflection(((0, 2), (2, 1)), (0, 0)))
 # => (1.6, 3.2)
