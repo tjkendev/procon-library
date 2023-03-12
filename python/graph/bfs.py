29fcf65090dca6589cba0f8701eaa0549c004768
tjkendev <jake-basu@hotmail.co.jp>
2023-03-12 23:21:17 +0900

    update some files

--- python/graph/bfs.py
+++ python/graph/bfs.py
@@ -1,11 +1,12 @@
 # G[v]: 頂点vに隣接する頂点list
 # N: 頂点数
+# s: 開始頂点
 from collections import deque
 
-def bfs(N, G):
+def bfs(N, G, s):
     dist = [-1] * N
-    que = deque([0])
-    dist[0] = 0
+    que = deque([s])
+    dist[s] = 0
     while que:
         v = que.popleft()
         d = dist[v]
--- python/math/pow.py
+++ python/math/pow.py
@@ -2,8 +2,8 @@
 # builtin functionのpow(x, n, mod)と同じ
 def fast_mod_pow(x, n, mod):
     r = 1
-    while n>0:
-        if n&1:
+    while n > 0:
+        if n & 1:
             r = (r * x) % mod
         x = (x * x) % mod
         n >>= 1
